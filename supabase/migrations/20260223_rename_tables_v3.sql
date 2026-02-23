3cdb2ee508f8ba7db8dab585d5119bd442e224c2
Keisuke0301 <osawakesukee@gmail.com>
2026-02-23 12:28:59 +0900

    Update 20260223_rename_tables_v3.sql

--- supabase/migrations/20260223_rename_tables_v3.sql
+++ supabase/migrations/20260223_rename_tables_v3.sql
@@ -1,8 +1,23 @@
--- テーブル名の変更
-ALTER TABLE chore_records_gachaprizes RENAME TO chore_gacha_prizes;
-ALTER TABLE chore_records_gachaprizes RENAME CONSTRAINT chore_records_gachaprizes_pkey TO chore_gacha_prizes_pkey;
+-- 1. ガチャ景品マスタテーブルの名称整理
+DO $$ 
+BEGIN
+    IF EXISTS (SELECT FROM pg_tables WHERE schemaname = 'public' AND tablename = 'chore_records_gachaprizes') THEN
+        ALTER TABLE chore_records_gachaprizes RENAME TO chore_gacha_prizes;
+    ELSIF EXISTS (SELECT FROM pg_tables WHERE schemaname = 'public' AND tablename = 'chore_gachaprizes') THEN
+        ALTER TABLE chore_gachaprizes RENAME TO chore_gacha_prizes;
+    ELSIF EXISTS (SELECT FROM pg_tables WHERE schemaname = 'public' AND tablename = 'gacha_prizes') THEN
+        ALTER TABLE gacha_prizes RENAME TO chore_gacha_prizes;
+    END IF;
+END $$;
+
 COMMENT ON TABLE chore_gacha_prizes IS 'ガチャの景品マスターテーブル';
 
-ALTER TABLE chore_user_inventory RENAME TO chore_records_prizes;
-ALTER TABLE chore_user_inventory RENAME CONSTRAINT chore_user_inventory_pkey TO chore_records_prizes_pkey;
+-- 2. 在庫管理テーブルの名称整理
+DO $$ 
+BEGIN
+    IF EXISTS (SELECT FROM pg_tables WHERE schemaname = 'public' AND tablename = 'chore_user_inventory') THEN
+        ALTER TABLE chore_user_inventory RENAME TO chore_records_prizes;
+    END IF;
+END $$;
+
 COMMENT ON TABLE chore_records_prizes IS 'ユーザーが獲得した報酬の在庫管理テーブル';
